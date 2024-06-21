22aeb737d8738ceed94a4336eadb31ddaf1db02f
emeraldingg <59565612+emeraldingg@users.noreply.github.com>
2024-06-21 19:32:53 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: {
+            globals: globals.node,
+            sourceType: "module",
+            ecmaVersion: "latest"
+        },
+        rules: {
+            "indent": ["error", 4],
+            "quotes": ["error", "double"],
+            "arrow-spacing": ["warn", { "before": true, "after": true }],
+            "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+            "semi": ["error", "always"],
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+];
